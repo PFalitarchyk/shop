595c82f350b2bf4e8fc7495f8bd4a74eb68f8965
Petr Falitarchyk <epam.petr.falitarchyk@gmail.com>
2018-03-18 19:39:13 +0300

    generate components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product-list/product/product.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ProductComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
